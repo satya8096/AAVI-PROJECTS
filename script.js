dbb1ad4a9757797eae1daf308980b9bc26b55f80
satya8096 <21MH5A0326@acoe.edu.in>
2023-09-02 01:37:01 +0530

    Mobile View

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+const navBar = document.querySelector(".get-nav");
+
+const bars = document.querySelector(".bars i");
+
+const xmark = document.querySelector(".xmark-icon i");
+
+
+bars.onclick = ()=>{
+    navBar.style.left = "-4rem"
+}
+
+xmark.onclick = ()=>{
+    navBar.style.left = "-34rem"
+}
